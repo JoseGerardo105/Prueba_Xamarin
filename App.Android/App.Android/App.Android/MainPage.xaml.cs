b9a9a4626428e3eae3d8abf3f180141f9ecdd8f5
José Gerardo Gómez López <josegomezlop@gmail.com>
2023-05-01 22:12:32 -0500

    Xamarin solution

---
+++ App.Android/App.Android/App.Android/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using App.Android.Modelo;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App.Android
+{
+    public partial class MainPage : ContentPage
+    {
+        private string url = "https://localhost:7072/api/Users";
+        HttpClient client = new HttpClient();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            string contenido = await client.GetStringAsync(url);
+            IEnumerable<UsuarioModel> lista = JsonConvert.DeserializeObject<IEnumerable<UsuarioModel>>(contenido);
+            ltusuario.ItemsSource = new ObservableCollection<UsuarioModel>(lista);
+            base.OnAppearing();
+        }
+    }
+}
